dc40fb84a89f6cace95102e55662ddf399482451
Raye <160432415+Dempserayo@users.noreply.github.com>
2026-02-10 12:01:06 -0500

    Add /time/{iso_code} endpoint to return current time based on country code; include error handling for unsupported codes

--- curso-fastapi-project/main.py
+++ curso-fastapi-project/main.py
@@ -1,16 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 app = FastAPI()
 
+country_timezone = {
+    "CO": "America/Bogota",
+    "MX": "America/Mexico_City",
+    "AR": "America/Argentina/Buenos_Aires",
+    "BR": "America/Sao_Paulo",
+    "PE": "America/Lima",
+}
 
-#Decorador @app 
 @app.get("/")
 async def root():
-    return {"message":"hola mundo"}
+    return("Hola mundo")
 
+@app.get("/time/{iso_code}")
+async def time(iso_code: str):
+    iso = iso_code.upper()
+    timezone_str = country_timezone.get(iso)
 
-@app.get("/test")
-async def hora():
-    hora = datetime.now()
-    return {"Hora: ": hora}
+    if not timezone_str:
+        raise HTTPException(
+            status_code=404,
+            detail="Código de país no soportado"
+        )
+
+    tz = ZoneInfo(timezone_str)
+    return { "country": iso, "timezone": timezone_str, "time": datetime.now(tz).isoformat()}
