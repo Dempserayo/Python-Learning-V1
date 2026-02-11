e456fd8e1f71a6c89c4619e03d9e7c31a6625318
Raye <160432415+Dempserayo@users.noreply.github.com>
2026-02-11 15:56:08 -0500

    Add customer, transaction, and invoice endpoints; integrate models for data handling

--- curso-fastapi-project/main.py
+++ curso-fastapi-project/main.py
@@ -3,6 +3,12 @@ import zoneinfo
 from fastapi import FastAPI
 from datetime import datetime
 
+from models import Customer
+from models import Transaction
+from models import Invoice
+
+
+
 app = FastAPI()
 
 country_timezone = {
@@ -17,9 +23,23 @@ country_timezone = {
 async def root():
     return("Hola mundo")
 
+
 @app.get("/time/{iso_code}")
 async def time(iso_code: str):
     iso = iso_code.upper()
     timezone_str = country_timezone.get(iso)
     tz = zoneinfo.ZoneInfo(timezone_str)
     return {"time": datetime.now(tz),}
+
+
+@app.post("/customers")
+async def create_customer(customer_data: Customer):
+    return customer_data
+
+@app.post("/transaction")
+async def create_transaction(transaction_data: Transaction):
+    return transaction_data
+
+@app.post("/invoice")
+async def create_invoice(invoice_data: Invoice):
+    return invoice_data
